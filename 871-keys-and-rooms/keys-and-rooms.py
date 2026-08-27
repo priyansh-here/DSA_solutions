aaa5c93728c0a2d54bf679d342a9c18345bf6587
priyansh-here <dixitpriyanshu24@gmail.com>
2026-08-27 11:10:41 +0530

    Time: 0 ms (100.00%) | Memory: 20.6 MB (10.85%) - LeetSync

---
+++ 871-keys-and-rooms/keys-and-rooms.py
@@ -0,0 +1,12 @@
+class Solution:
+    def canVisitAllRooms(self, rooms):
+        seen = set()
+
+        def dfs(room):
+            seen.add(room)
+            for key in rooms[room]:
+                if key not in seen:
+                    dfs(key)
+
+        dfs(0)
+        return len(seen) == len(rooms)
